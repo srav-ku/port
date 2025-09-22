22ca8db8493cda9360182c295bf5aaa2b7c20a21
srav <109066452+srav-ku@users.noreply.github.com>
2025-09-22 12:11:46 +0530

    Add files via upload

--- client/src/adminMain.tsx
+++ client/src/adminMain.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import AdminApp from './admin/AdminApp';
 import './index.css';
 
+// Import setup utilities for development
+import './lib/setupAdmin';
+import './lib/testFirestore';
+import './lib/debugFirestore';
+import './lib/enhancedFirestore';
+
 ReactDOM.createRoot(document.getElementById('admin-root')!).render(
   <React.StrictMode>
     <AdminApp />
